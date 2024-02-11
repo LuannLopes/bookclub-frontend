cc37eee9830e65a564e797b9a5d2390021322c9d
LuannLopes <luanlopesofc@gmail.com>
2024-02-11 13:28:21 -0300

    feat: implement menu

---
+++ src/components/molecules/SearchBar.js
@@ -0,0 +1,21 @@
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+
+export const SearchBar = () => {
+  return (
+    <Flex w="478px" h="52px" bg="brand.greyLight" borderRadius="12px">
+      <InputGroup>
+        <InputLeftElement h="100%">
+          <SearchIcon color="brand.greyDark" />
+        </InputLeftElement>
+        <Input
+          borderWidth="0px"
+          w="100%"
+          h="100%"
+          placeholder="Digite o nome do livro ou autor"
+          focusBorderColor="transparent"
+        />
+      </InputGroup>
+    </Flex>
+  )
+}
