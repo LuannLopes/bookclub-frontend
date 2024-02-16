012e2cc82e7dcaa10a19fb6607f833c620ad32c1
LuannLopes <luanlopesofc@gmail.com>
2024-02-16 15:02:51 -0300

    feat: feat: favorites list

--- src/modules/authenticated/screens/BookDetail/index.js
+++ src/modules/authenticated/screens/BookDetail/index.js
@@ -81,16 +81,18 @@ export const BookDetailScreen = () => {
   return (
     <Flex flexDir="column">
       <NavBar />
-
       <Flex
-        flexDir="row"
+        flexDir={['column', 'row']}
+        alignItems={['center', 'flex-start']}
+        justifyContent={['center', 'flex-start']}
         mt={['24px', '48px']}
         w="100%"
+        maxW={'100vw'}
         paddingX={['24px', '48px', '80px', '112px']}
       >
         <Flex
-          w={['238px']}
-          h={['358px']}
+          w={['170px', '238px']}
+          h={['256px', '358px']}
           backgroundImage={`url(${data?.data?.book?.cover_url})`}
           backgroundSize="cover"
           backgroundPosition="center"
@@ -98,7 +100,12 @@ export const BookDetailScreen = () => {
           borderRadius={['12px']}
         />
 
-        <Flex w="70%" mx="48px" flexDir="column">
+        <Flex
+          mt={['24px', '0px']}
+          w={['100%', '70%']}
+          mx={['0px', '48px']}
+          flexDir="column"
+        >
           <Text.ScreenTitle fontSize="24px">
             {data?.data?.book?.name}
           </Text.ScreenTitle>
@@ -107,7 +114,12 @@ export const BookDetailScreen = () => {
           </Text>
 
           <Text.ScreenTitle mt="16px">Informações</Text.ScreenTitle>
-          <Flex mt="4px" w="100%" flexDir="row" justifyContent="space-between">
+          <Flex
+            mt="4px"
+            w="100%"
+            flexDir={['column', 'row']}
+            justifyContent={['flex-start', 'space-between']}
+          >
             <Text fontSize="14px" color="brand.greyDark">
               Categoria: {data?.data?.book?.category?.name}
             </Text>
@@ -119,12 +131,17 @@ export const BookDetailScreen = () => {
               {new Date(data?.data?.book?.release_date).getFullYear()}
             </Text>
           </Flex>
-          <Text.ScreenTitle mt="12px">Sinopse</Text.ScreenTitle>
-          <Text fontSize="12px" color="brand.greyDark">
+          <Text.ScreenTitle mt="16px">Sinopse</Text.ScreenTitle>
+          <Text maxW="80%" fontSize="12px" mt="4px" color="brand.greyDark">
             {data?.data?.book?.synopsis}
           </Text>
         </Flex>
-        <Flex>
+        <Flex
+          justifyContent={['center', 'flex-start']}
+          alignItems={['center', 'flex-start']}
+          w={['100%', 'auto']}
+          mt={['24px', '0px']}
+        >
           <Button
             isLoading={
               isLoading ||
