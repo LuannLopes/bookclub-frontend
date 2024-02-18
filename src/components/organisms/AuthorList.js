c8149744238e13c7c25380c04caf6907f70c6620
LuannLopes <luanlopesofc@gmail.com>
2024-02-18 11:48:30 -0300

    feat: search implemented

--- src/components/organisms/AuthorList.js
+++ src/components/organisms/AuthorList.js
@@ -1,8 +1,8 @@
 import { Flex, Spinner } from '@chakra-ui/react'
 import { Text } from 'components/atoms'
-import { BookCard } from 'components/molecules'
+import { AuthorCard } from 'components/molecules'
 
-export const BookList = ({ title, data, isLoading }) => {
+export const AuthorList = ({ data, isLoading }) => {
   return (
     <Flex
       flexDir="column"
@@ -15,7 +15,7 @@ export const BookList = ({ title, data, isLoading }) => {
         }
       }}
     >
-      <Text.ScreenTitle>{title}</Text.ScreenTitle>
+      <Text.ScreenTitle>Autores</Text.ScreenTitle>
 
       <Flex mt={['12px', '24px']} flexDir="row">
         {isLoading && (
@@ -23,14 +23,16 @@ export const BookList = ({ title, data, isLoading }) => {
             <Spinner />
           </Flex>
         )}
-        {!data ||
-          (!isLoading && data?.length === 0 && (
+        {(!isLoading && data?.length === 0) ||
+          (!data && (
             <Flex alignItems="center" justifyContent="center" h="30px">
-              <Text>Nenhum livro encontrado.</Text>
+              <Text>Nenhum autor encontrado.</Text>
             </Flex>
           ))}
         {data &&
-          data?.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
+          data?.map((item) => (
+            <AuthorCard key={`author_${item.id}`} {...item} />
+          ))}
       </Flex>
     </Flex>
   )
